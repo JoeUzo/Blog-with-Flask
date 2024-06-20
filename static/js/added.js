98a0d3819db7b8f7692a30a08588e26a23881f0a
Joe <joeuzoproject@gmail.com>
2024-06-20 11:49:09 +0100

    update added.js

--- static/js/added.js
+++ static/js/added.js
@@ -1,12 +1,16 @@
-  function toggleCommentBox(id) {
-    var commentBox = document.getElementById(id);
-    var addCommentBtn = document.getElementById('addCommentBtn' + id.replace( /^\D+/g, ''));
+ function toggleCommentBox(id) {
+   var commentBox = document.getElementById(id);
+   var addCommentBtn = document.getElementById('addCommentBtn' + id.replace( /^\D+/g, ''));
+   if (commentBox.style.display == 'none') {
+     commentBox.style.display = 'block';
+     ClassicEditor.create(document.querySelector('#editor' + id.replace( /^\D+/g, '')));
+   } else {
+     commentBox.style.display = 'none';
+     addCommentBtn.style.display = 'block';
+   }
+ }
 
-    if (commentBox.style.display == 'none') {
-      commentBox.style.display = 'block';
-      ClassicEditor.create(document.querySelector('#editor' + id.replace( /^\D+/g, '')));
-    } else {
-      commentBox.style.display = 'none';
-      addCommentBtn.style.display = 'block';
-    }
-  }
+ function confirmDelete() {
+   return confirm('Are you sure you want to delete this post?');
+ }
+
